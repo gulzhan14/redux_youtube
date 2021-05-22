dd063943860250f2a6983798ae33a551730a784f
gulzhan14 <burgkhankyzygulzhan@gmail.com>
2021-05-22 15:34:20 +0600

    redux_youtube

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import ReduxPromise from 'redux-promise'
+import reducer from './reduser'
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore) 
+
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={createStoreWithMiddleware(reducer)}>
+  
+  
+  <Router>
     <App />
+    </Router>
+  
+  </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reduser/youtube.js
@@ -0,0 +1,10 @@
+  
+import {GET_VIDEO_BY_NAME} from '../action'
+
+export default function(state=[], action){
+    switch(action.type){
+        case GET_VIDEO_BY_NAME:
+            return [action.payload]
+    }
+    return state
+}
